01609f70c2a1de1de3426d0c7569f7c74b726a25
Yukun Zhang <ykzhang1211@g.ucla.edu>
2023-03-19 14:00:53 -0700

    add front end plot

--- src/app.py
+++ src/app.py
@@ -13,3 +13,31 @@ movies['Released_Year'] =movies['Released_Year'].str.extract('(\d+)')
 movies = movies.dropna()
 movies['Released_Year'] = movies['Released_Year'].astype(int)
 
+# Setup app and layout/frontend
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+server = app.server
+
+app.layout = dbc.Container([
+    html.H1('IMDB Statistics Benchmarking Dashboard'),
+    dbc.Row([
+        dbc.Col([ html.Label('Select the Baseline Year:'),
+            dcc.Dropdown(
+                id='year1',
+                value=2016,  
+                options=[{'label': yr1, 'value': yr1} for yr1 in movies['Released_Year']),
+            html.Label('Select the Second Year:'),
+            dcc.Dropdown(
+                id='year2',
+                value=2017,  
+                options=[{'label': yr2, 'value': yr2} for yr2 in movies['Released_Year'])
+                ],
+                md=4),
+        dbc.Col([
+            dbc.Row([
+                dbc.Col()]),
+            html.Iframe(
+                id='boxplot',
+                style={'border-width': '0', 'width': '100%', 'height': '400px'})])])])
+
+
+
