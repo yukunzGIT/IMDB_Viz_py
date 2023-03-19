05edf2ad57c12764e141827553e35127583c0d48
Yukun Zhang <ykzhang1211@g.ucla.edu>
2023-03-19 14:10:03 -0700

    update plot1

--- src/app.py
+++ src/app.py
@@ -11,7 +11,7 @@ movies['Runtime'] = movies['Runtime'].str.extract('(\d+)').astype(int)
 movies = movies.loc[:,['Director', 'Released_Year', 'IMDB_Rating']]
 movies['Released_Year'] =movies['Released_Year'].str.extract('(\d+)')
 movies = movies.dropna()
-movies['Released_Year'] = movies['Released_Year'].astype(int)
+movies['Released_Year'] = movies['Released_Year'].astype(str)
 
 # Setup app and layout/frontend
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
@@ -23,12 +23,12 @@ app.layout = dbc.Container([
         dbc.Col([ html.Label('Select the Baseline Year:'),
             dcc.Dropdown(
                 id='year1',
-                value=2016,  
+                value='2016',  
                 options=[{'label': yr1, 'value': yr1} for yr1 in movies['Released_Year']),
             html.Label('Select the Second Year:'),
             dcc.Dropdown(
                 id='year2',
-                value=2017,  
+                value='2017',  
                 options=[{'label': yr2, 'value': yr2} for yr2 in movies['Released_Year'])
                 ],
                 md=4),
@@ -36,26 +36,23 @@ app.layout = dbc.Container([
             dbc.Row([
                 dbc.Col()]),
             html.Iframe(
-                id='boxplot',
+                id='boxplot1',
                 style={'border-width': '0', 'width': '100%', 'height': '400px'})])])])
 
 
 # Set up callbacks/backend
 @app.callback(
-    Output('boxplot', 'srcDoc'),
+    Output('boxplot1', 'srcDoc'),
     Input('year1', 'value'),
     Input('year2', 'value')
     )
 def plot_altair(year1, year2):
 
-    mo
-
-    scatter = alt.Chart(movies).mark_point().encode(
-        x=alt.X(xcol, scale=alt.Scale(domain=[x_min, x_max])),
-        y=alt.Y(ycol, scale=alt.Scale(domain=[y_min, y_max])),
-        tooltip=['Series_Title','Genre','Released_Year', 'Runtime','IMDB_Rating'])
-    scatter = scatter.properties(title = 'Scatter Plot of ' + xcol + ' and ' + ycol)
-    output = scatter + text + scatter.transform_regression(xcol,ycol).mark_line(color = 'red')
+    boxplot1 = alt.Chart(movies.query("Released_Year == @year1 | Released_Year == @year2"), width=400, height=300).mark_boxplot().encode(
+        x=alt.X('IMDB_Rating', scale=alt.Scale(domain=[7.4, 9.4]), axis=alt.Axis(title='IMDB rating')),
+        y=alt.Y('Released_Year', axis=alt.Axis(title='Released year'))
+    ).properties(title = 'Benchmarking Boxplot Release Year and IMDB Rating')
+    output = boxplot1 + boxplot1.mark_point(size = 10)
     return output.to_html()
 
 if __name__ == '__main__':
